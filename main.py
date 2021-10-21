feb6f4d0301dd2ed367666c75689ff5d931aea74
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-21 06:56:16 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+##Taking examples of two python lists.
+
+##Take examples of two lists.
+
+list1 = [2,4,0,7,6]
+list2 = [1,0,9,7,6]
+
+##the statement for condition is.
+
+check = any(element in list2 for element in list1)
